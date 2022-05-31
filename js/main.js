ae57b26617cb6c0c03f1fb89cc771f1885d4d97d
Nicholas Fernandez <Nickfer610@gmail.com>
2022-05-31 20:37:25 +0000

    added css

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// /* global data */
+
+// take image from input-photo (url) and show it on photo-preview //
+
+// make querySelector to target the input-photo image //
+// addEventListener to listen for inputs of //
+
+// make 2 query selectors to take photo and show it//
+
+// var imageUrlInput = document.querySelector('.url');
+
+// imageUrlInput.addEventListener('input', showImage);
+
+// function showImage(event) {
+//   if (imageUrlInput.value !== '') {
+//     imageUrls.push(imageUrlInput.value);
+//   }
+//   imageUrlInput.value = '';
+
+// }
