d18c332dd40f8ab105811fa29aa4588a03062a3e
Nicholas Fernandez <Nickfer610@gmail.com>
2022-05-31 22:55:32 +0000

    added js entry form

--- js/main.js
+++ js/main.js
@@ -1,20 +1,41 @@
-// /* global data */
+/* global data */
 
 // take image from input-photo (url) and show it on photo-preview //
 
 // make querySelector to target the input-photo image //
-// addEventListener to listen for inputs of //
+// addEventListener to listen for inputs //
 
 // make 2 query selectors to take photo and show it//
 
-// var imageUrlInput = document.querySelector('.url');
+var imageInput = document.querySelector('.url');
+var imageOutput = document.querySelector('.photoPreview');
 
-// imageUrlInput.addEventListener('input', showImage);
+imageInput.addEventListener('input', inputImage);
+imageOutput.addEventListener('input', outputImage);
 
-// function showImage(event) {
-//   if (imageUrlInput.value !== '') {
-//     imageUrls.push(imageUrlInput.value);
-//   }
-//   imageUrlInput.value = '';
+function inputImage(event) {
+  if (imageInput.value !== '') {
+    data.push(imageInput.value);
+  }
+  imageInput.value = '';
+  outputImage();
+}
 
-// }
+function outputImage(event) {
+
+}
+
+var entryForm = document.querySelector('#new-entry-form');
+
+function handleEntryForm(event) {
+  event.preventDefault();
+  // var entryFull = {
+  //   title: entryForm.elements.title.value,
+  //   photoURL: entryForm.elements.url.value,
+  //   notes: entryForm.elements.notes.value
+  // };
+  // console.log('entryFull', entryFull);
+  entryForm.reset();
+}
+
+entryForm.addEventListener('submit', handleEntryForm);
