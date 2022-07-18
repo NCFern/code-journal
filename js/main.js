c0b96746d4c7fd8937ba2128dc2010a39b99cf6f
Nicholas Fernandez <Nickfer610@gmail.com>
2022-07-18 23:32:20 +0000

    shows photo in placeholder

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 /* global data */
+
+var $photo = document.querySelector('#user-photo');
+var $showImage = document.querySelector('.placeholder');
+
+$photo.addEventListener('input', showPicture);
+
+function showPicture(event) {
+  if ($photo.value !== '') {
+    $showImage.setAttribute('src', $photo.value);
+  } else {
+    $showImage.setAttribute('src', 'images/placeholder-image-square.jpg');
+  }
+}
