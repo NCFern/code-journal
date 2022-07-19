7e71530c844cea9369d0cc2dcd879da5d9c63bf9
Nicholas Fernandez <Nickfer610@gmail.com>
2022-07-19 00:55:49 +0000

    submit function working

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 
 var $photo = document.querySelector('#user-photo');
 var $showImage = document.querySelector('.placeholder');
+var $form = document.querySelector('#new-form');
 
 $photo.addEventListener('input', showPicture);
 
@@ -11,4 +12,20 @@ function showPicture(event) {
   } else {
     $showImage.setAttribute('src', 'images/placeholder-image-square.jpg');
   }
+}
+
+$form.addEventListener('submit', submitForm);
+
+function submitForm(event) {
+  event.preventDefault();
+  var submitEntry = {
+    entryId: data.nextEntryId,
+    title: $form.elements.title.value,
+    imgURL: $form.elements.photo.value,
+    notes: $form.elements.notes.value
+  };
+  data.entries.unshift(submitEntry);
+  data.nextEntryId++;
+  $form.reset();
+  $showImage.setAttribute('src', 'images/placeholder-image-square.jpg');
 }
